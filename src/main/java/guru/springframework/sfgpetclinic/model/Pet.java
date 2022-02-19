6e35468a7d96b36f62331b6f31e3ce939412104c
cjnorth_513 <cjnorthington@gmail.com>
2022-02-19 18:32:56 -0500

    Adding Index page, controllers and services

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.aspectj.weaver.patterns.PerTypeWithin;
+
+public class Pet extends BaseEntitiy{
+
+    private String petType;
+    private String owner;
+
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.services;
+
+import java.util.Set;
+
+public class PetService implements CrudService{
+    @Override
+    public Set findAll() {
+        return null;
+    }
+
+    @Override
+    public Object findById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object object) {
+
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+}
